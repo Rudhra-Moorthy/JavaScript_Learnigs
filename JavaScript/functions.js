aef3f5ab54e190c516c91ad69c3b1b285f0745cc
Rudhra Moorthy <rudhram54@gmail.com>
2025-07-22 11:26:39 +0530

    Update functions.js

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,4 +1,4 @@
-/* let person = {
+let person = {
     name: "John",
     age: 30
 }
@@ -13,32 +13,32 @@ function printDetails(name, age) {
 printDetails(person.name, person.age);
 printDetails("Deva", 30);
 printDetails("Rudhra", 21);
-printDetails("Bala Kumar", 21); */
+printDetails("Bala Kumar", 21);
 
 // Default Parameters
 
-/* function details(name = "Guest", age = 30) {
+function details(name = "Guest", age = 30) {
     console.log(`Hi, I am ${name} and my age is ${age}`);
 }
 
 details("Rudhra", 21);
 details(undefined, 21);
 details("Deva", undefined);  // either use undefined or don't use undefined
-details(undefined, undefined); */
+details(undefined, undefined);
 
 // Default value for variables ->  By using || operator
 
-/* let empId ;
+let empId ;
 let newId = empId || "BZF100";
-console.log(newId); */
+console.log(newId);
 
 // Return and Non-return Type functions
 
-/* function findArea(l, b) {
+function findArea(l, b) {
     return l * b;
 }
 
-console.log(findArea(5,6)); */
+console.log(findArea(5,6));
 
 /* Higher Order Function ->  the function which has the another function as a argument, which function is called Higher Order Function
 Call Back Function -> the funtion that is passed as a argument to the another function, which is called as Call Back Function */
